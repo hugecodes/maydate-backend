1592204cacfd8dd38818ffaaf1e5dc77deb56e1e
Mike Yassa <myassa@hugeinc.com>
2018-02-09 11:19:59 -0500

    making it work

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const app = express()
 
+var accountSid = process.env.TWILIOAUTH
+var authToken = process.env.TWILIOTOKEN
+
+
+const client = require('twilio')(accountSid, authToken);
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/call', (req, res) => {
 
+	client.calls
+	  .create({
+	    url: 'http://demo.twilio.com/docs/voice.xml',
+	    to: '+1' + req.query.number,
+	    from: '+16476961768',
+	  })
+	  .then(call => process.stdout.write(call.sid));
+	
 	console.log("test");
-	console.log(req.query.number);
 	res.send("got it");
 })
 
